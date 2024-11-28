3ff8ea07927f8f22ead8968fbfcb68b313ec06ae
akeda <akeda2@gmail.com>
2024-11-28 15:09:23 +0100

    /usr/bin/

--- inst.sh
+++ inst.sh
@@ -15,6 +15,6 @@ command -v cargo \
 pushd pueue
 	cargo build --release --locked || { echo "FAIL" ; exit 1;}
 popd
-sudo install -m 755 target/release/pueue target/release/pueued /usr/local/bin \
+sudo install -m 755 target/release/pueue target/release/pueued /usr/bin \
 	&& sudo pueue completions bash /usr/share/bash-completion/completions
 sudo cp utils/pueued.service /etc/systemd/user/ && systemctl enable --user pueued.service
