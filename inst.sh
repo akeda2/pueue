46d37cdeebca9260f4150f906f7fb5cb6d1f3092
akeda2 <akeda2@gmail.com>
2024-11-28 12:58:21 +0100

    even more modern inst

--- inst.sh
+++ inst.sh
@@ -5,11 +5,16 @@ cont() {
     echo
     [[ $REPLY =~ ^[Yy]$ ]]
 }
-
-command -v cargo || { cont "No cargo/rust. Install?" && curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh ; echo "logout, login and run again" ; exit 0;}
+# Look for cargo:
+command -v cargo \
+	|| { cont "No cargo/rust. Install?" \
+		&& curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh ;\
+		echo "logout, login and run again" ;\
+		exit 0;}
+# Build pueue:
 pushd pueue
 	cargo build --release --locked || { echo "FAIL" ; exit 1;}
 popd
-sudo cp target/release/pueue target/release/pueued /usr/local/bin && \
-sudo pueue completions bash /usr/share/bash-completion/completions
+sudo install -m 755 target/release/pueue target/release/pueued /usr/local/bin \
+	&& sudo pueue completions bash /usr/share/bash-completion/completions
 sudo cp utils/pueued.service /etc/systemd/user/ && systemctl enable --user pueued.service
